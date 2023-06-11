0148ddb00fa59b46187450d1c3353f7e78fc0f8b
vanshita00 <129608607+vanshita00@users.noreply.github.com>
2023-06-11 22:16:27 +0530

    Add files via upload

---
+++ Includes.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np
+import pickle
+import cv2
+from os import listdir
+from sklearn.preprocessing import LabelBinarizer
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation, Flatten, Dropout, Dense
+from keras import backend as K
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras.preprocessing import image
+from keras.preprocessing.image import img_to_array
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+#epochs and size of NN is defined
+
+EPOCHS = 25
+INIT_LR = 1e-3
+BS = 32
+default_image_size = tuple((256, 256))
+image_size = 0
+directory_root = 'C:/Users/Baba/Leaf_disease_detection/PlantVillage'
+width = 256
+height = 256
+depth = 3
